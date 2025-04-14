bc5dbed44185b4fa1d44f8fb251a900bb3a57f77
Carson Gusaas <cars@infra04-wg012.eduroam.wireless.umn.edu>
2025-04-14 17:27:49 -0500

    Changed detections, started adding cloud variables

--- FinalProject/src/FinalProject.cpp
+++ FinalProject/src/FinalProject.cpp
@@ -1,5 +1,5 @@
 #include "Particle.h"
-//#include "neopixel.h"
+#include "neopixel.h"
 
 SYSTEM_MODE(AUTOMATIC);
 SYSTEM_THREAD(ENABLED);
@@ -14,15 +14,15 @@ int motionPIN = D2; //motion sensor pin
 unsigned long int timeToToggle;
 int sensorDelay = 1500; //change as needed
 int detectionCount;
+bool detection;
 
 
-/*
 //iLED config:
 int PIXEL_COUNT = 1;
 #define PIXEL_PIN SPI // S0 Pin
 int PIXEL_TYPE = WS2812;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
-*/
+
 
 //int setModeFromString(String inputString); //cloud function calls this
 //int setTargetTempFromString(String inputString);
@@ -31,49 +31,69 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 void setup() {
 
   pinMode(motionPIN, INPUT);
-
   Serial.begin(9600);
+  strip.begin();
 
 
   timeToToggle = millis() + sensorDelay; // Initialize the timeToToggleLED variable
 
   detectionCount = 0;
+  detection = false;
+  
 
 
-   
+  Particle.variable("DetectionCount", detectionCount);
+  Particle.variable("DetectionTime", detectionTime); //Use some type of c++ function to record time
 
-  //Particle.variable("tempC", tempC);
   //Particle.variable("cV_targetTemp", targetTemp);
 
   //Particle.function("cF_SetMode", setModeFromString); //declares particle function that calls setModeFromString
   //Particle.function("cF_setTargetTemp", setTargetTempFromString);
 
-  //strip.begin(); //setup iLED
+
 
 }
 
 void loop() {
   unsigned long int currentTime = millis();  // get the current time
-
   int motionReading = digitalRead(motionPIN);
 
+
+  int PixelColorRed  = strip.Color(   0,  255,   0);
+  int PixelColorGreen  = strip.Color(   255,  0,   0);
+  int PixelOFF  = strip.Color(   0,  0,   0);
+
   //int lastReading = LOW;
-  
+   
+   
+  if(motionReading == HIGH) {  //Add OFF mode configurable through website
+    detection = true;
 
+    strip.setPixelColor(0, PixelColorRed);
+    strip.show();
 
-  //its detecting way too many times, despite the timer which is meant to fix it
+  } else {
+    detection = false;
+
+    strip.setPixelColor(0, PixelColorGreen);
+    strip.show();
+
+  }
+
   
   //if motion is detected and at least 1.5s have passed since last detection:
-  if(motionReading == HIGH && currentTime > timeToToggle) {
+  if(detection == true ) { //&& currentTime >= timeToToggle
       detectionCount++;
 
       Serial.print("MOTION DETECTED: ");
       Serial.println(detectionCount);
+      delay(2500);
 
-      timeToToggle += 5000; //test
+    
+      //timeToToggle += sensorDelay; //test
+
+      detection = false;
       
-
   }
 }
-
 
